9aea6a075170db656fb7d8b28e273b09425ef142
orxan666 <orxanallahyarov03@gmail.com>
2022-03-24 19:48:25 +0400

    24.03.2022

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import HeroSection from "../components/heroSection/HeroSection";
 import { Button, Icon, Label } from "semantic-ui-react";
 import Courses from "../courses/Courses";
-import Elementor from "../components/elementor/Elementor";
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <Courses />
-      <Elementor/>
     </div>
   );
 };
